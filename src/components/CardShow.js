1ba6396494149f42843b348290324d984d4043a4
aarik <roblesaarik@gmail.com>
2020-11-08 19:26:09 -0800

    added second random card

---
+++ src/components/CardShow.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class CardShow extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Card Name</h1>
+      </div>
+    );
+  }
+}
--- src/components/RandomCards.js
+++ src/components/RandomCards.js
@@ -3,30 +3,47 @@ import React from "react";
 class RandomCards extends React.Component {
   state = {
     card1: {},
+    card2: {},
   };
 
   componentDidMount() {
-    this.getRandomCards();
+    this.getRandomCard1();
+    this.getRandomCard2();
   }
 
-  getRandomCards() {
+  getRandomCard1() {
     fetch("https://db.ygoprodeck.com/api/v7/randomcard.php")
       .then((response) => response.json())
       .then((json) => this.setState({ card1: json }))
       .catch((error) => console.log(error));
   }
+  getRandomCard2() {
+    fetch("https://db.ygoprodeck.com/api/v7/randomcard.php")
+      .then((response) => response.json())
+      .then((json) => this.setState({ card2: json }))
+      .catch((error) => console.log(error));
+  }
   render() {
-    let image = this.state.card1.card_images;
-    console.log(this.state.card1.card_images);
-    console.log(image);
+    let image1 = this.state.card1.card_images;
+    let image2 = this.state.card2.card_images;
+    console.log(image2);
 
     return (
-      <div className="random">
-        <h3>{this.state.card1.name}</h3>
-        <img
-          src={image ? image[0].image_url_small : this.state.card1.name}
-          alt="no Pic"
-        />
+      <div>
+        <div className="random">
+          <h3>{this.state.card1.name}</h3>
+          <img
+            src={image1 ? image1[0].image_url_small : this.state.card1.name}
+            alt="no Pic"
+          />
+        </div>
+        <div className="random">
+          <h3>{this.state.card2.name}</h3>
+          <img
+            src={image2 ? image2[0].image_url_small : this.state.card2.name}
+            alt="no Pic"
+          />
+        </div>
       </div>
     );
   }
